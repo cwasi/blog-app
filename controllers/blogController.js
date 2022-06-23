d864819795cc2f4114c3ab67e75542ae566e0217
Francis Osei <oseifrancis673@gmail.com>
2022-06-23 23:53:43 +0100

    Add update Blog

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,5 +1,56 @@
+import multer from 'multer';
+import sharp from 'sharp';
 import catchAsync from '../utils/catchAsync.js';
 import Blog from '../models/blogModel.js';
+import AppError from '../utils/appError.js';
+
+const multerStorage = multer.memoryStorage();
+
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('imaga')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image. Please upload only image', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+const uploadBlogImage = upload.single('photo');
+
+const resizeBlogImage = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `Blog-${req.params.id}-${Date.now()}.jpeg`;
+
+  await sharp(req.file.buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/blogImages/${req.file.filename}`);
+
+  next();
+});
+
+const UpdateBlog = catchAsync(async (req, res, next) => {
+  const doc = await Blog.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  if (!doc) {
+    return next(new AppError('NO document found with that Id', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { doc },
+  });
+});
 
 const getAllBlogs = catchAsync(async (req, res, next) => {
   const doc = await Blog.find();
@@ -18,9 +69,15 @@ const createBlog = catchAsync(async (req, res, next) => {
   res.status(201).json({
     status: 'success',
     data: {
-      data: doc
+      data: doc,
     },
   });
 });
 
-export { getAllBlogs, createBlog };
+export {
+  uploadBlogImage,
+  resizeBlogImage,
+  getAllBlogs,
+  createBlog,
+  UpdateBlog,
+};
--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -1,5 +1,11 @@
 import express from 'express';
-import { getAllBlogs, createBlog } from '../controllers/blogController.js';
+import {
+  getAllBlogs,
+  createBlog,
+  uploadBlogImage,
+  resizeBlogImage,
+  UpdateBlog,
+} from '../controllers/blogController.js';
 import { protect, restrictTo } from '../controllers/authController.js';
 
 const router = express.Router();
@@ -7,6 +13,8 @@ const router = express.Router();
 router
   .route('/')
   .get(getAllBlogs)
-  .post(protect, restrictTo('admin','user'), createBlog);
+  .post(protect, restrictTo('admin', 'user'), createBlog);
+
+router.route('/:id').patch(uploadBlogImage, resizeBlogImage, UpdateBlog);
 
 export default router;
