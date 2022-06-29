d78e3abacb8100bea7fa78c6e63761bc504eac4e
Francis Osei <oseifrancis673@gmail.com>
2022-06-29 08:07:31 +0100

    Add getComment, updateComment, delectComment

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -1,9 +1,10 @@
 import Comment from '../models/commentModel.js';
+import AppError from '../utils/appError.js';
 import catchAsync from '../utils/catchAsync.js';
 
 const setCommentAndUserIds = catchAsync(async (req, res, next) => {
   if (!req.body.comment) req.body.comment = req.params.commentId;
-  if (!req.body.user) req.body.user = req.id;
+  if (!req.body.user) req.body.user = req.user.id;
 
   next();
 });
@@ -30,4 +31,56 @@ const createComment = catchAsync(async (req, res, next) => {
   });
 });
 
-export { setCommentAndUserIds, createComment, getAllComments };
+const getComment = catchAsync(async (req, res, next) => {
+  const doc = await Comment.findById(req.params.id);
+
+  if (!doc) {
+    return next(new AppError('No document found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: { doc },
+  });
+});
+
+const updateComment = catchAsync(async (req, res, next) => {
+  const doc = await Comment.findOneAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+
+  if (!doc) {
+    return next(new AppError('No document found with that ID', 404));
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      data: doc,
+    },
+  });
+});
+
+const deleteComment = catchAsync(async (req, res, next) => {
+  const doc = await Comment.findByIdAndDelete(req.params.id);
+
+  if (!doc) {
+    return next(new AppError('No document found with that ID', 404));
+  }
+
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
+
+export {
+  setCommentAndUserIds,
+  createComment,
+  getAllComments,
+  getComment,
+  updateComment,
+  deleteComment,
+};
--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -4,6 +4,9 @@ import {
   setCommentAndUserIds,
   createComment,
   getAllComments,
+  getComment,
+  updateComment,
+  deleteComment,
 } from '../controllers/commentController.js';
 
 const router = express.Router({ mergeParams: true });
@@ -14,5 +17,11 @@ router
   .route('/')
   .get(getAllComments)
   .post(restrictTo('user'), setCommentAndUserIds, createComment);
+
+router
+  .route('/:id')
+  .get(getComment)
+  .patch(restrictTo('user','admin'), updateComment)
+  .delete(restrictTo('user','admin'), deleteComment);
 
 export default router;
